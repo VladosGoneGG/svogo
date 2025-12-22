0935478032c98d0b6cd2488aa3dfefad3b564992
Vladislav Medelean <vlad93gm@gmail.com>
2025-12-22 20:37:45 +0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header'
+
+const Home = () => {
+	return (
+		<div className=' max-w-300 min-[1200px]:mx-auto'>
+			<Header />
+			<main className=''>a</main>
+		</div>
+	)
+}
+
+export default Home
